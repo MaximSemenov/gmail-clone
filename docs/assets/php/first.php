cd49e69678a7ee9055ddb1db4dd736b57bf1253c
MaximSemenov <maxim.n.semenov@gmail.com>
2018-03-04 22:35:10 -0500

    There is first.php file with very simple code for combining Angular-PHP-MySQL

--- docs/assets/php/first.php
+++ docs/assets/php/first.php
@@ -1,5 +1,21 @@
 <?php 
+header("Access-Control-Allow-Origin: http://localhost:4200");
 
-echo 'Hello There. I am here!';
+
+$link = mysqli_connect("127.0.0.1:3306", "root", "", "gmail")
+    or die("There is a problem with connection to the database: " . mysql_error() . ":(");
+
+    $result = mysqli_query($link, "SELECT * from gmail");
+
+    $arr = [];
+    
+    while ($obj = mysqli_fetch_object($result)) {
+    
+        array_push($arr, $obj);
+    
+    }
+    echo json_encode($arr);
+
+
 
 ?>

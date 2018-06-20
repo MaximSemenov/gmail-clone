cd746eec85d16d84e382b6ab1d0591351fed05f9
MaximSemenov <maxim.n.semenov@gmail.com>
2018-06-19 23:56:44 -0400

    Changes in index.php for new dependency injection container class.

--- server-side-php-files/index.php
+++ server-side-php-files/index.php
@@ -8,22 +8,23 @@ require './php/http/routing/route.php';
 require './php/http/routing/request.php';
 require './php/Dependencies.php';
 require './php/MailDataBase.php';
+require './php/Container/Container.php';
 
 $router = new AppRouter();
+
 
 $router->register(new Route('#mail/(?<method>getMail)#', Request::METHOD_GET, 'Mail'));
 $router->register(new Route('#mail/(?<method>transferMail)#', Request::METHOD_GET, 'Mail'));
 $router->register(new Route('#mail/(?<method>unreadMail)#', Request::METHOD_GET, 'Mail'));
 
-
-
-
 $route = $router->resolve(Request::createFromGlobals());
-
 $controller = $route->getTarget();
-$instance = new $controller(new MailDataBase ());
+$container = new Container();
+$instance = $container->make($controller);
 $method = $route->getAction();
 $result = $instance->$method();
 
 echo $result;
+
+
 

5e4f217f928f41c46f25a370aaad23da412899bf
MaximSemenov <maxim.n.semenov@gmail.com>
2018-07-19 23:01:42 -0400

    New rigister methods for AbstractKernel class and process logic

--- server-side-php-files/framework/Http/AbstractKernel.php
+++ server-side-php-files/framework/Http/AbstractKernel.php
@@ -3,8 +3,10 @@ declare (strict_type = 1);
 
 namespace Framework\Http;
 
-use Framework\Container\Container;
 use Framework\Http\Kernel;
+use Framework\Http\PlainResponse;
+use Framework\Container\Container;
+use Framework\Container\ServiceProvider;
 
 abstract class AbstractKernel implements Kernel
 {
@@ -20,10 +22,57 @@ abstract class AbstractKernel implements Kernel
         $this->container = $container;
         $this->container->bind(Container::class, $this);
         $this->container->bind(Kernel::class, $this);
+        $this->registerDefaultProviders();
     }
     public function process(Request $request) : ? Responce
     {
+        $this->container->bind(Request::class, $request);
+        $this->registerProviders();
+        /**
+         * @var Router
+         */
+        $router = $this->container->make(Router::class);
+        /**
+         * @var Route
+         */
+        $route = $router->resolve($request);
+        if ($route === null) {
+            return $this->createNotFoundResponce();
+        }
+        $controller = $this->container->make($route->getTarget()->getControllerName());
+        $response = $this->container->call($controller, $route->getTarget()->getMethodName());
+        return $response;
+    }
 
+    public function LoadProviders() : void
+    {
+
+    }
+
+    public function registerDefaultProviders() : void
+    {
+        $this->register($this->defaultProviders);
+    }
+
+    private function registerProviders(array $providers) : void
+    {
+        $this->register($this->providers);
+    }
+
+    private function register(array $providers)
+    {
+        foreach ($providers as $provider) {
+            $instance = $this->container->make($provider);
+            if (!($instance instanceof ServiceProvider)) {
+                throw new \Exception("Service provider {$provider} must be an instance of Framework\Container\ServiceProvider");
+            }
+            $instance = register($this->container);
+        }
+    }
+
+    private function createNotFoundResponce() : Response
+    {
+        return new PlainResponse('Not Found', Responce::STATUS_NOT_FOUND);
     }
 
 }

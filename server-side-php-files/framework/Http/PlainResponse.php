6f5cc0405c082e46345f00ccb5b43189253f1a09
MaximSemenov <maxim.n.semenov@gmail.com>
2018-07-21 13:54:21 -0400

    Plain Response class

--- server-side-php-files/framework/Http/PlainResponse.php
+++ server-side-php-files/framework/Http/PlainResponse.php
@@ -1,20 +1,53 @@
 <?php
-declare (strict_type = 1);
+declare(strict_types = 1);
 
 namespace Framework\Http;
 
-use Framework\Http\Response;
-
-
 class PlainResponse implements Response
 {
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var int
+     */
+    private $status;
+
+    /**
+     * @var array
+     */
+    private $headers;
+
     public function __construct(string $content = '', int $status = Response::STATUS_OK, array $headers = [])
     {
-
+        $this->content = $content;
+        $this->status = $status;
+        $this->headers = $headers;
     }
 
-    public function send() : void
+    public function send(): void
     {
+        $this->sendHeaders();
+        $this->sendContent();
 
+        if (function_exists('fastcgi_finish_request')) {
+            fastcgi_finish_request();
+        }
+    }
+
+    private function sendHeaders(): void
+    {
+        $this->headers[] = "HTTP/1.1 {$this->status}";
+
+        foreach ($this->headers as $header) {
+            header($header);
+        }
+    }
+
+    private function sendContent()
+    {
+        echo $this->content;
     }
 }

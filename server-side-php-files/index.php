c718993986d0a9b38d643ed9ee02aecc3bc47ba9
MaximSemenov <maxim.n.semenov@gmail.com>
2018-07-22 23:22:06 -0400

    Clean up comments

--- server-side-php-files/index.php
+++ server-side-php-files/index.php
@@ -11,23 +11,6 @@ use Framework\Http\Request;
 use Framework\Http\Routing\Route;
 use Framework\Http\Routing\Router;
 
-// header("Access-Control-Allow-Origin: http://localhost:4200");
-
-// $router = new Router();
-// $router->register(new Route('#mail/(?<method>getMail)#', Request::METHOD_GET, new Target(App\Controllers\MailController::class, 'getMail')));
-// $router->register(new Route('#mail/(?<method>transferMail)#', Request::METHOD_GET, new Target(App\Controllers\MailController::class, 'transferMail')));
-// $router->register(new Route('#mail/(?<method>unreadMail)#', Request::METHOD_GET, new Target(App\Controllers\MailController::class, 'unreadMail')));
-
-// $request = Request::createFromGlobals();
-// $route = $router->resolve($request);
-// $controller = $route->getControllerName();
-// $container = new Container();
-// $container->bind(Request::class, $request);
-// $instance = $container->make($controller);
-// $result = $container->call($instance, $route->getAction());
-
-// echo $result;
-
 $request = Request::createFromGlobals();
 $kernel = new AppKernel(new SimpleContainer);
 $response = $kernel->process($request);

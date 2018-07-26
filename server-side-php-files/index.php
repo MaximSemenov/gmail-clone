00845e9a55d5d8309b5763ca37edef7abd57aa73
MaximSemenov <maxim.n.semenov@gmail.com>
2018-07-25 22:24:31 -0400

    clean up for index.php

--- server-side-php-files/index.php
+++ server-side-php-files/index.php
@@ -5,11 +5,7 @@ require 'vendor/autoload.php';
 
 use App\AppKernel;
 use Framework\Container\SimpleContainer;
-
-use Framework\Http\Target;
 use Framework\Http\Request;
-use Framework\Http\Routing\Route;
-use Framework\Http\Routing\Router;
 
 $request = Request::createFromGlobals();
 $kernel = new AppKernel(new SimpleContainer);

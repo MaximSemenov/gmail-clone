ada5c5b9298a15756d6159697ef84be2eed48fca
MaximSemenov <maxim.n.semenov@gmail.com>
2018-07-28 23:31:59 -0400

    index.php file a bug fix

--- server-side-php-files/index.php
+++ server-side-php-files/index.php
@@ -3,11 +3,13 @@ declare(strict_types = 1);
 
 require 'vendor/autoload.php';
 
-use App\AppKernel;
+use App\GmailKernel;
 use Framework\Container\SimpleContainer;
 use Framework\Http\Request;
+use Framework\Http\Response;
 
 $request = Request::createFromGlobals();
-$kernel = new AppKernel(new SimpleContainer);
+$kernel = new GmailKernel(new SimpleContainer);
+/** @var Response $response */
 $response = $kernel->process($request);
 $response->send();
